4fea08dc077c27781b4e9d323c1485f27fcbc07c
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2018-08-30 10:46:08 +0200

    Move content to other function

--- fs/ext4/xattr_extacl.c
+++ fs/ext4/xattr_extacl.c
@@ -1,16 +1,28 @@
 #include <linux/syscalls.h>
 #include "extacl.h"
 
+#define LOOKUP_FLAGS 0
+
+static ssize_t path_extacl_get_file(const char __user *pathname,
+    const struct extacl __user *extacl, size_t size)
+{
+  return -ENOSYS;
+}
+
+static ssize_t path_extacl_set_file(const char __user *pathname,
+    const struct extacl __user *extacl, size_t size)
+{
+  return -ENOSYS;
+}
+
 SYSCALL_DEFINE3(extacl_get_file, const char __user *, pathname,
     const struct extacl __user *, extacl, size_t, size)
 {
-  struct path path;
-  return -ENOSYS;
+  return path_extacl_get_file(pathname, extacl, size);
 }
 
 SYSCALL_DEFINE3(extacl_set_file, const char __user *, pathname,
     const struct extacl __user *, extacl, size_t, size)
 {
-  struct path path;
-  return -ENOSYS;
+  return path_extacl_set_file(pathname, extacl, size);
 }
