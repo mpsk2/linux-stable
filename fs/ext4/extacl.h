2cc262280185d14390ecd15e3469a097a37d68e0
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2018-08-31 11:35:43 +0200

    Basic functions for extacl

--- fs/ext4/extacl.h
+++ fs/ext4/extacl.h
@@ -5,8 +5,11 @@
  */
 
 #include <linux/extacl_xattr.h>
+#include <linux/extacl.h>
 
 #ifndef _EXTACL_H
 #define _EXTACL_H
+
+int ext4_init_extacl(handle_t *handle, struct inode *inode, struct inode *dir);
 
 #endif /* _EXTACL_H */
--- include/linux/extacl_xattr.h
+++ include/linux/extacl_xattr.h
@@ -11,4 +11,23 @@
 #include <uapi/linux/extacl_xattr.h>
 #include <linux/extacl.h>
 
+static inline size_t
+extacl_xattr_size(int count)
+{
+  return sizeof(extacl_xattr_header_t) + count * sizeof(extacl_xattr_entry_t);
+}
+
+static inline int
+extacl_xattr_count(size_t size)
+{
+  if (size < sizeof(extacl_xattr_header_t)) {
+    return -1;
+  }
+  size -= sizeof(extacl_xattr_header_t);
+  if (size % sizeof(extacl_xattr_entry_t)) {
+    return -1;
+  }
+  return size / sizeof(extacl_xattr_entry_t);
+}
+
 #endif  /* __LINUX_EXTACL_XATTR_H */
